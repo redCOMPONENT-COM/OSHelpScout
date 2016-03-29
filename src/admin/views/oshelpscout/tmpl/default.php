70fca92fe15a8a0fbccdcc4b5442f9f1e76b1cb7
Anderson Grudtner Martins <anderson@ostraining.com>
2016-03-29 11:42:24 -0300

    Add basic admin view

---
+++ src/admin/views/oshelpscout/tmpl/default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package   OSHelpScout
+ * @contact   www.alledia.com, hello@alledia.com
+ * @copyright 2016 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
