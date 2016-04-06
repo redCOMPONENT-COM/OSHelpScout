11d28245c8ce1e5d1b688c722d49c4595268957f
Anderson Grudtner Martins <anderson@ostraining.com>
2016-04-06 18:34:38 -0300

    Add "member since" info to helpscout app

--- src/site/views/hsdynamicapp/tmpl/default.php
+++ src/site/views/hsdynamicapp/tmpl/default.php
@@ -6,18 +6,31 @@
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
+use Carbon\Carbon;
+
 defined('_JEXEC') or die();
 ?>
 
 <?php if (!empty($this->customers)) : ?>
     <?php foreach ($this->customers as $customer) : ?>
-        <p><?php echo JText::_('COM_OSHELPSCOUT_USERNAME'); ?>: <?php echo $customer->username; ?></p>
+        <p><strong><?php echo JText::_('COM_OSHELPSCOUT_USERNAME'); ?>: <?php echo $customer->username; ?></strong></p>
         <ul>
-            <li><?php echo JText::_('COM_OSHELPSCOUT_ID'); ?>: <?php echo $customer->id; ?></li>
-            <li><?php echo JText::_('COM_OSHELPSCOUT_NAME'); ?>: <?php echo $customer->name; ?></li>
-            <li><?php echo JText::_('COM_OSHELPSCOUT_EMAIL'); ?>: <?php echo $customer->email; ?></li>
+            <li><strong><?php echo JText::_('COM_OSHELPSCOUT_ID'); ?></strong>: <?php echo $customer->id; ?></li>
+            <li><strong><?php echo JText::_('COM_OSHELPSCOUT_NAME'); ?></strong>: <?php echo $customer->name; ?></li>
+            <li><strong><?php echo JText::_('COM_OSHELPSCOUT_EMAIL'); ?></strong>: <?php echo $customer->email; ?></li>
+            <li>
+                <?php
+                $timezone = JFactory::getConfig()->get('offset');
+                $date = Carbon::parse($customer->registerDate);
+                $date->timezone = new DateTimeZone($timezone);
+                ?>
+                <strong><?php echo JText::_('COM_OSHELPSCOUT_REGISTER_DATE'); ?></strong>:&nbsp;
+                <span data-uk-tooltip="{pos:'top'}" title="<?php echo $date->format(JText::_('COM_OSHELPSCOUT_DATE_FORMAT')); ?>">
+                    <?php echo $date->diffForHumans(); ?>
+                </span>
+            </li>
             <?php if (!empty($customer->groups)) : ?>
-                <li><?php echo JText::_('COM_OSHELPSCOUT_GROUPS'); ?>:
+                <li><strong><?php echo JText::_('COM_OSHELPSCOUT_GROUPS'); ?></strong>:
                     <ul>
                         <?php foreach ($customer->groups as $group) : ?>
                             <li><?php echo $group->title; ?></li>
@@ -28,5 +41,5 @@ defined('_JEXEC') or die();
         </ul>
     <?php endforeach; ?>
 <?php else : ?>
-    <p><b><?php echo JText::_('COM_OSHELPSCOUT_NOT_A_MEMBER'); ?></b></p>
+    <p><strong><?php echo JText::_('COM_OSHELPSCOUT_NOT_A_MEMBER'); ?></strong></p>
 <?php endif; ?>
