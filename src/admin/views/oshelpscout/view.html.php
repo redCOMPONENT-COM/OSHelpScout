9a442f225e3e5db97a1b50c98f78b4e0d43cd493
Anderson Grudtner Martins <anderson@ostraining.com>
2016-04-19 16:22:59 -0300

    HSCOUT-18 - Add title and config button to the admin view

--- src/admin/views/oshelpscout/view.html.php
+++ src/admin/views/oshelpscout/view.html.php
@@ -10,5 +10,14 @@ defined('_JEXEC') or die;
 
 class OSHelpScoutViewOshelpscout extends JViewLegacy
 {
+    public function display($tpl = null)
+    {
+        $this->addToolbar();
+    }
 
+    protected function addToolbar()
+    {
+        JToolBarHelper::title(JText::_('COM_OSHELPSCOUT') . ': ' . JText::_('COM_OSHELPSCOUT_DASHBOARD'));
+        JToolBarHelper::preferences('com_oshelpscout', '450');
+    }
 }
